28008521207ce9222db1c5350d9d9acca1784937
Cuong Bui <cuongbphv@gmail.com>
2018-10-28 15:02:09 +0700

    finish first demo chat app

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
+const mongoose = require('mongoose');
 
 // define variable
 var app = express();
